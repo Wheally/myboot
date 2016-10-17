086e1adef7306bec3bd05b43d83fc9f0fd8ac9e6
wenxiaoqian <404190475@qq.com>
2016-10-17 18:44:24 +0800

    添加根据不同环境信息打包

--- src/main/java/com/hzm/boot/controller/HomeController.java
+++ src/main/java/com/hzm/boot/controller/HomeController.java
@@ -1,11 +1,15 @@
 package com.hzm.boot.controller;
 
 import com.hzm.boot.domain.User;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -15,17 +19,26 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class HomeController {
 
+    private static Map<String,Thread> threadMap = new ConcurrentHashMap<String, Thread>();
+
     private AtomicLong count = new AtomicLong();
 
-    @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    public String hello(){
-        System.out.println("hello:111");
-
-        while(count.get() < 10000000000000L){
-            count.addAndGet(1);
+    @RequestMapping(value = "/wait",method = RequestMethod.GET)
+    public String wait(@PathVariable String userId){
+        Thread currentThread = Thread.currentThread();
+        threadMap.put(userId, currentThread);
+        try {
+            currentThread.wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        return "Hello World";
+    }
 
-        System.out.println("hello:222");
+    @RequestMapping(value = "/notify",method = RequestMethod.GET)
+    public String notify(@PathVariable String userId){
+
+        threadMap.get(userId).notify();
         return "Hello World";
     }
 }
