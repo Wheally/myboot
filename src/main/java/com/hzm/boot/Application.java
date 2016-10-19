f968a396411ff899028720de86991c42fad6b225
wenxiaoqian <404190475@qq.com>
2016-10-19 11:19:59 +0800

    添加Druid控制台，添加原生的Servlet,Filter支持

--- src/main/java/com/hzm/boot/Application.java
+++ src/main/java/com/hzm/boot/Application.java
@@ -1,13 +1,18 @@
 package com.hzm.boot;
 
+import com.hzm.boot.servlet.DemoServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@ServletComponentScan
 public class Application implements EmbeddedServletContainerCustomizer {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/hzm/boot/servlet/DemoServlet.java
@@ -0,0 +1,38 @@
+package com.hzm.boot.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by WXQ on 2016/10/19.
+ */
+@WebServlet(urlPatterns="/demoServlet/*", description="Servlet的说明")
+public class DemoServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println(">>>>>>>>>>doPost()<<<<<<<<<<<");
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello World</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>这是：demoServlet</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
