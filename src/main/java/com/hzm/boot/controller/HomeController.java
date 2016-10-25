7bfb5077b2823eb6b04a774c1c7b23191d2e3918
wenxiaoqian <404190475@qq.com>
2016-10-25 18:37:06 +0800

    SpringBoot的logback集成ELK日志收集

--- src/main/java/com/hzm/boot/controller/HomeController.java
+++ src/main/java/com/hzm/boot/controller/HomeController.java
@@ -19,7 +19,7 @@ public class HomeController {
         currentThread.setName("thread-" + userId);
         synchronized(currentThread) {
             try {
-                currentThread.wait();
+                currentThread.wait(5000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
